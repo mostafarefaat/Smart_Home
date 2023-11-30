e033a3cd3be71546cad70aaa017d39bd430a4af4
mostafa <mostafarefaat456@gmail.com>
2023-11-30 10:58:27 +0200

    Test case

--- Slave_/Master_/Debug/ECUAL/Servo Motor/servo.d
+++ Slave_/Master_/Debug/ECUAL/Servo Motor/servo.d
@@ -5,12 +5,16 @@ ECUAL/Servo Motor/servo.d ECUAL/Servo Motor/servo.o: \
  ../ECUAL/Servo\ Motor/../../MCAL/DIO\ Driver/../../utilities/types.h \
  ../ECUAL/Servo\ Motor/../../MCAL/DIO\ Driver/../../utilities/bit_manipulation.h \
  ../ECUAL/Servo\ Motor/servo.h \
+ ../ECUAL/Servo\ Motor/../../MCAL/TIMER0/timer0.h \
+ ../ECUAL/Servo\ Motor/../../MCAL/TIMER0/../../utilities/registers.h \
+ ../ECUAL/Servo\ Motor/../../MCAL/TIMER0/../../utilities/types.h \
+ ../ECUAL/Servo\ Motor/../../MCAL/TIMER0/../../utilities/bit_manipulation.h \
+ d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
  d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
  d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h
 
 ../ECUAL/Servo\ Motor/../../MCAL/DIO\ Driver/dio.h:
 
@@ -22,6 +26,16 @@ ECUAL/Servo Motor/servo.d ECUAL/Servo Motor/servo.o: \
 
 ../ECUAL/Servo\ Motor/servo.h:
 
+../ECUAL/Servo\ Motor/../../MCAL/TIMER0/timer0.h:
+
+../ECUAL/Servo\ Motor/../../MCAL/TIMER0/../../utilities/registers.h:
+
+../ECUAL/Servo\ Motor/../../MCAL/TIMER0/../../utilities/types.h:
+
+../ECUAL/Servo\ Motor/../../MCAL/TIMER0/../../utilities/bit_manipulation.h:
+
+d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
 d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
 d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
@@ -31,5 +45,3 @@ d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\incl
 d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
 d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
-
-d:\microship_studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
